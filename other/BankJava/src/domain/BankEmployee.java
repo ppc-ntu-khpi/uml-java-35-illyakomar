40c9c8b219c345bb02d7d6ae1bd58e3caafe76ab
Illya <illya-komar@ukr.net>
2020-05-05 13:11:03 +0300

    Add bank

---
+++ other/BankJava/src/domain/BankEmployee.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class BankEmployee {
+
+    private int idBankEmployee;
+
+    private String nameEmployee;
+
+    private String position;
+
+    public BankEmployee(String nameEmployee, String addressEmployee, String position) {
+    }
+
+    public String getNameEmployee() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setNameEmployee(String Employee) {
+    }
+
+    public String getPosition() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setPosition(String position) {
+    }
+}
